5cc6ae11d60ac5fc0fba5df9d5aaaf3fe3526037
Leo Wong <leoelicos@gmail.com>
2023-03-12 17:23:23 +1100

    Prettier, Remove comments

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,32 +1,25 @@
-const butInstall = document.getElementById("buttonInstall");
+const butInstall = document.getElementById('buttonInstall')
 
 window.addEventListener('beforeinstallprompt', (event) => {
+  window.deferredPrompt = event
 
-    // Store the triggered events
-    window.deferredPrompt = event;
-
-    // Remove the hidden class from the button.
-    butInstall.classList.toggle('hidden', false);
-  });
+  butInstall.classList.toggle('hidden', false)
+})
 
 butInstall.addEventListener('click', async () => {
-  
-  const promptEvent = window.deferredPrompt;
+  const promptEvent = window.deferredPrompt
 
   if (!promptEvent) {
-   return;
+    return
   }
 
-  // Show prompt
-  promptEvent.prompt();
-  
-  // Reset the deferred prompt variable, it can only be used once.
-  window.deferredPrompt = null;
-  
-  butInstall.classList.toggle('hidden', true);
-});
+  promptEvent.prompt()
+
+  window.deferredPrompt = null
+
+  butInstall.classList.toggle('hidden', true)
+})
 
 window.addEventListener('appinstalled', (event) => {
-  // Clear prompt
-  window.deferredPrompt = null;
-}); 
+  window.deferredPrompt = null
+})
